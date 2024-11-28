3dc198d2ec7b5d52986a7da2505b7bce1955822a
Juan Pablo <juanpablovalencia749@gmail.com>
2024-11-27 19:46:05 -0500

    Fix header spacing to improve visualization and allow clicking the LinkedIn icon for redirection

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
     return (
         <div className='static w-full py-5 md:mt-10 md:py-0'>
             <motion.div variants={fadeIn('up', 0.5)} initial="hidden" animate="show" exit="hidden">
-                <div className="container justify-between mx-auto md:flex">
+                <div className="container justify-around mx-auto md:flex">
                     <Link href="/">
                         <h1 className="my-3 text-3xl md:text-4xl text-center text-white md:text-lef">Juan Pablo
                             <span className="text-secondary"> Valencia</span>
