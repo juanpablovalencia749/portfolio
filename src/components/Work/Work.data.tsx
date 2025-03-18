04c1afd153eafe907690aa311e2038d58828f33b
ZV <juanpablovalencia749@gmail.com>
2025-03-18 14:04:44 -0500

    Made changes to the portfolio; still a work in progress. Continuing to work on it alongside other tasks.

--- src/components/Work/Work.data.tsx
+++ src/components/Work/Work.data.tsx
@@ -1,87 +1,89 @@
-import { SiTailwindcss, SiGithub, SiReact, SiExpress, SiTypescript   } from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiGithub,
+  SiReact,
+  SiExpress,
+  SiTypescript,
+} from "react-icons/si";
 
 export const cardContent = [
   {
     id: 0,
     title: "Calendar",
     state: "Go",
-    description:
-      "Calendar with login, powered by React and Express",
-      projectUrl:"https://calendar749.netlify.app",
+    description: "Calendar with login, powered by React and Express",
+    projectUrl: "https://calendar749.netlify.app",
     imageUrl: "/calendar.jpg",
     skills: [
       {
-        icon: <SiReact size="40" />
+        icon: <SiReact size="40" />,
       },
       {
-        icon: <SiExpress  size="40" />
+        icon: <SiExpress size="40" />,
       },
-
-    ]
+    ],
   },
   {
     id: 1,
     title: "To Do List",
     state: "Go",
-    description:
-      "To-do list project crafted with React",
-      projectUrl:"https://todo-list749.netlify.app",
+    description: "To-do list project crafted with React",
+    projectUrl: "https://todo-list749.netlify.app",
     imageUrl: "/todo-list.jpg",
     skills: [
       {
-        icon: <SiReact size="40" />
+        icon: <SiReact size="40" />,
       },
       {
-        icon: <SiGithub size="40" />
+        icon: <SiGithub size="40" />,
       },
       {
-        icon: <SiTypescript size="40" />
+        icon: <SiTypescript size="40" />,
       },
-    ]
+    ],
   },
   {
     id: 2,
     title: "Cinema",
-    state:'Go',
-    description:
-      "React cinema project with public API",
-    projectUrl:"https://cinema749.netlify.app",
+    state: "Go",
+    description: "React cinema project with public API",
+    projectUrl: "https://cinema749.netlify.app",
     imageUrl: "/movies.jpg",
     skills: [
       {
-        icon: <SiReact size="40" />
+        icon: <SiReact size="40" />,
       },
       {
-        icon: <SiTailwindcss size="40" />
+        icon: <SiTailwindcss size="40" />,
       },
       {
-        icon: <SiTypescript size="40" />
+        icon: <SiTypescript size="40" />,
       },
-    ]
+    ],
   },
-  {
-    id: 3,
-    title: "Threads Clone",
-    state:'On Progress',
-    description:
-      "Modern social clone with React and Express",
-    projectUrl:"",
-    imageUrl: "/threads.jpg",
-    skills: [
-      {
-        icon: <SiReact size="40" />
-      },
-      {
-        icon: <SiTailwindcss size="40" />
-      },
-      {
-        icon: <SiExpress size="40" />
-      },
-      {
-        icon: <SiTypescript size="40" />
-      },
-    ]
-  },
+  // {
+  //   id: 3,
+  //   title: "Threads Clone",
+  //   state:'On Progress',
+  //   description:
+  //     "Modern social clone with React and Express",
+  //   projectUrl:"",
+  //   imageUrl: "/threads.jpg",
+  //   skills: [
+  //     {
+  //       icon: <SiReact size="40" />
+  //     },
+  //     {
+  //       icon: <SiTailwindcss size="40" />
+  //     },
+  //     {
+  //       icon: <SiExpress size="40" />
+  //     },
+  //     {
+  //       icon: <SiTypescript size="40" />
+  //     },
+  //   ]
+  // },
   // {
   //   id: 4,
   //   title: "E-Commerce React",
